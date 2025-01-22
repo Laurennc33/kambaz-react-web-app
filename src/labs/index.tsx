0bbd27a7361626da017125859f32a9f0840cdd87
Laurennc33 <laurennc3323@gmail.com>
2025-01-22 13:59:54 -0500

    first commit

---
+++ src/labs/index.tsx
@@ -0,0 +1,17 @@
+import Lab1 from "./lab1";
+import Lab2 from "./lab2";
+import Lab3 from "./lab3";
+
+
+export default function Labs() {
+    return (
+        <div>
+            <h1>Labs</h1>
+            <Lab1 />
+            <Lab2 />
+            <Lab3 />
+    
+        </div>
+    );
+}
+
