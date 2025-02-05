e34297d8ee23785d833f71f1992637bfc551b465
Laurennc33 <laurennc3323@gmail.com>
2025-02-05 16:52:34 -0500

    fixed errors

--- src/Kambaz/navigation.tsx
+++ src/Kambaz/navigation.tsx
@@ -3,8 +3,7 @@ import { ListGroup } from "react-bootstrap";
 import { IoPersonCircleOutline } from "react-icons/io5";
 import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
-import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
-import { FaInbox, FaRegCircleUser, FaBook } from "react-icons/fa6";
+import { FaInbox, FaBook } from "react-icons/fa6";
 import { HiBeaker } from "react-icons/hi";
 
 
